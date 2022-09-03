ec42f7cab47a52653755ec6b36940fd158b4869c
henrysmile2071 <henrychen.chc@gmail.com>
2022-09-03 13:39:12 +0800

    feat: modify index.hbs and app.js to display todos

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const mongoose = require('mongoose') //require mongoose
 const exphbs = require('express-handlebars')
-
+const Todo = require('./models/todo')
 const app = express()
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) //setting connection to mongoDB
 
@@ -21,7 +21,10 @@ app.set('view engine', 'hbs')
 
 //Setting routes
 app.get('/', (req, res) => {
-  res.render('index')
+  Todo.find()
+  .lean()
+  .then(todos => res.render('index', { todos }))
+  .catch(error => console.error(error))
 })
 
 //Open and listen to server port
