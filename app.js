560fea2169becdb96988d85543e51eff51432eac
henrysmile2071 <henrychen.chc@gmail.com>
2022-09-03 12:25:50 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 //Import required
 const express = require('express')
+const mongoose = require('mongoose') //require mongoose
+
 const app = express()
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) //setting connection to mongoDB
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongoDB error!')
+})
+
+db.once('open', () => {
+  console.log('mongoDB connected!')
+})
 
 //Setting routes
 app.get('/', (req, res) => {
