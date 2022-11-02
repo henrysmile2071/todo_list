beb82590b7f23cd5a6f3866c011bef5a03795535
henrysmile2071 <henrychen.chc@gmail.com>
2022-11-03 00:16:15 +0800

    feat: bcrypt login

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,7 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const User = require('../models/user')
-
+const bcrypt = require('bcryptjs')
 module.exports = app => {
   //initialize passport module
   app.use(passport.initialize())
@@ -11,12 +11,14 @@ module.exports = app => {
     User.findOne({ email })
       .then(user => {
         if (!user) {
-          return done(null, false, req.flash('error', {message:'Email not registered!'}))
+          return done(null, false, req.flash('error', { message: 'Email not registered!' }))
         }
-        if (user.password !== password) {
-          return done(null, false, req.flash('error', {message:'Email or Password incorrect.'}))
-        }
-        return done(null, user)
+        return bcrypt.compare(password, user.password).then(isMatch => {
+          if (!isMatch) {
+            return done(null, false, req.flash('error', { message: 'Email or Password incorrect.' }))
+          }
+          return done(null, user)
+        })
       })
       .catch(err => done(err, false))
   }))
